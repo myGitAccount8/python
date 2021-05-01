207eaf079e19147b7877371ea890b7826a55eb0a
Murat <ndilmurat724@gmail.com>
2021-04-30 20:34:20 -0400

    Initial commit

---
+++ 01_Demo_readFile/readFile.py
@@ -0,0 +1,11 @@
+# import csv
+# with open('Employees_table.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file)
+#     for line in csv_reader:
+#         print(line)
+
+
+myFile = open("/Users/15715/Desktop/presentation_05042021/demo/test.txt")
+print (myFile.read())
+myFile.close()
+
